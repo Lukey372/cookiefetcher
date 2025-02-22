cf23085a259717110ce17741484e4021bc2b25f0
Lukey372 <100325058+Lukey372@users.noreply.github.com>
2025-02-22 22:23:28 +1100

    Update fetch_cookies.py

--- app/fetch_cookies.py
+++ app/fetch_cookies.py
@@ -11,7 +11,6 @@ def get_cookies():
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--window-size=1920,1080")
     # Try running without headless mode if detection is an issue:
-    options.add_argument("--headless=new")
     options.add_argument("--disable-infobars")
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
                          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36")
