b42c2d62440711fccebf22b0acfce0765dbc4480
Lukey372 <100325058+Lukey372@users.noreply.github.com>
2025-02-22 21:57:28 +1100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    
+    # import the routes after creating the app
+    from app.server import bp as main_bp
+    app.register_blueprint(main_bp)
+    
+    return app
 
